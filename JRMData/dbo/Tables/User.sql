70677d315af22f17d44a2e990cbf8b66f270397e
Joshua Constante <joshuaconstante2197@hotmail.com>
2021-04-17 14:29:14 -0400

    Register DB Tables
    Create DB Tables for management system

---
+++ JRMData/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+    [Id] NCHAR(128) NOT NULL PRIMARY KEY, 
+    [FirstName] NCHAR(50) NULL, 
+    [LastName] NCHAR(50) NULL, 
+    [EmailAddress] NVARCHAR(256) NULL, 
+    [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
