d57fbd9d897d87ad1af0e097c27100d5c758d7f7
Joshua Constante <joshuaconstante2197@hotmail.com>
2021-04-29 18:41:47 -0400

    Fix Typo on SalesViewModel

--- JRMDesktopUI/ViewModels/SalesViewModel.cs
+++ JRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -11,7 +11,7 @@ namespace JRMDesktopUI.ViewModels
     public class SalesViewModel : Screen
     {
         private BindingList<string> _products;
-        private string _itemQuantity;
+        private int _itemQuantity;
         private BindingList<string> _cart;
 
         public BindingList<string> Products
@@ -24,13 +24,13 @@ namespace JRMDesktopUI.ViewModels
             }
         }
 
-        public string ItemQuantity
+        public int ItemQuantity
         {
             get { return _itemQuantity; }
             set 
             { 
                 _itemQuantity = value;
-                NotifyOfPropertyChange(() => Products);
+                NotifyOfPropertyChange(() => ItemQuantity);
             }
         }
 
