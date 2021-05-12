0d775f0bb81d7954ef5ebdacf3d26841f5664f9d
Joshua Constante <joshuaconstante2197@hotmail.com>
2021-05-12 17:12:16 -0400

    Add product model, api endpoint, and db data

--- JRMData/dbo/Tables/Product.sql
+++ JRMData/dbo/Tables/Product.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[Product]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [ProductName] VARBINARY(100) NOT NULL, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProductName] NVARCHAR(MAX) NOT NULL, 
     [Description] NVARCHAR(MAX) NOT NULL,
     [RetailPrice] money NOT NULL,
+    [QuantityInStock] INT NOT NULL DEFAULT 1,
     [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
-    [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate()
+    [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate() 
+    
 )
---
+++ JRMDataManager/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using JRMDataManager.Library.DataAccess;
+using JRMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace JRMDataManager.Controllers
+{
+    [Authorize]
+    public class ProductController : ApiController
+    {
+        public List<ProductModel> Get()
+        {
+            ProductData data = new ProductData();
+            
+            return data.GetProducts();
+
+        }
+
+    }
+}
